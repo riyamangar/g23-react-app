2f6f7ea8ab4b8147598fb4ea045346a20bb4225e
Riya Mangar <mangarriya3@gmail.com>
2025-05-02 16:53:45 +0530

    Intial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>My react app</h1>
+      <h2>my react Implementation with CI/CD</h2>
+      <h3>with Github Action</h3>
+    </div>
+  )
+}
+
+export default App
